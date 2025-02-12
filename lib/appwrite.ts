83fb3440ec4740a9be8045f4a2e30c23a0f73573
callmenk36 <callmenk36@gmail.com>
2025-02-12 23:28:52 +0530

    data base added

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,83 +1,98 @@
-import { Account, Avatars, Client, OAuthProvider } from 'react-native-appwrite'
-import * as Linking from 'expo-linking'
-import { openAuthSessionAsync } from 'expo-web-browser'
+import {
+    Account,
+    Avatars,
+    Client,
+    Databases,
+    OAuthProvider,
+} from 'react-native-appwrite';
+import * as Linking from 'expo-linking';
+import { openAuthSessionAsync } from 'expo-web-browser';
 
 export const config = {
     platform: 'com.nk.trueEstate',
     endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
     projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
-}
+    databaseId: process.env.EXPO_PUBLIC_APPWRITE_DATABASE,
+    galleriesCollectionId:
+        process.env.EXPO_PUBLIC_APPWRITE_GALLERIES_COLLECTION_ID,
+    reviewsCollectionId: process.env.EXPO_PUBLIC_APPWRITE_REVIEWS_COLLECTION_ID,
+    agentCollectionId: process.env.EXPO_PUBLIC_APPWRITE_AGENT_COLLECTION_ID,
+    propertiesCollectionId:
+        process.env.EXPO_PUBLIC_APPWRITE_PROPERTIES_COLLECTION_ID,
+};
 
-export const client = new Client()
+export const client = new Client();
 
 client
     .setEndpoint(config.endpoint!)
     .setProject(config.projectId!)
-    .setPlatform(config.platform!)
+    .setPlatform(config.platform!);
 
-export const avatar = new Avatars(client)
-export const account = new Account(client)
+export const avatar = new Avatars(client);
+export const account = new Account(client);
+export const database = new Databases(client);
 
 export async function login() {
     try {
-        const redirectUri = Linking.createURL('/')
+        const redirectUri = Linking.createURL('/');
 
         const response = await account.createOAuth2Token(
             OAuthProvider.Google,
             redirectUri
-        )
+        );
 
-        if (!response) throw new Error('Failed to login')
+        if (!response) throw new Error('Failed to login');
 
         const browserResult = await openAuthSessionAsync(
             response.toString(),
             redirectUri
-        )
+        );
 
-        if (browserResult.type !== 'success') throw new Error('Failed to login')
+        if (browserResult.type !== 'success')
+            throw new Error('Failed to login');
 
-        const url = new URL(browserResult.url)
+        const url = new URL(browserResult.url);
 
-        const secret = url.searchParams.get('secret')?.toString()
-        const userId = url.searchParams.get('userId')?.toString()
+        const secret = url.searchParams.get('secret')?.toString();
+        const userId = url.searchParams.get('userId')?.toString();
 
-        if (!userId || !secret) throw new Error('Failed to login')
+        if (!userId || !secret) throw new Error('Failed to login');
 
-        const session = await account.createSession(userId, secret)
+        const session = await account.createSession(userId, secret);
 
-        if (!session) throw new Error('Failed to create session')
+        if (!session) throw new Error('Failed to create session');
 
-        return true
+        return true;
     } catch (error) {
-        console.log(error)
-        return false
+        console.log(error);
+        return false;
     }
 }
 
 export async function logout() {
     try {
-        await account.deleteSession('current')
-        return true
+        await account.deleteSession('current');
+        return true;
     } catch (error) {
-        console.log(error)
-        return false
+        console.log(error);
+        return false;
     }
 }
 
 export async function getCurrentUser() {
     try {
-        const response = await account.get()
+        const response = await account.get();
         if (response.$id) {
-            const userAvatar = avatar.getInitials(response.name)
+            const userAvatar = avatar.getInitials(response.name);
 
             return {
                 ...response,
                 avatar: userAvatar.toString(),
-            }
+            };
         }
-        return response
+        return response;
     } catch (error) {
-        console.log(error)
-        return false
+        console.log(error);
+        return false;
     }
 }
