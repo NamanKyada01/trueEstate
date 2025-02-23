ee8904ab297c91582a156864b13acb7abef04fa3
callmenk36 <callmenk36@gmail.com>
2025-02-23 19:37:43 +0530

    App completed   !!!!!!!!! :)

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -4,19 +4,21 @@ import {
     Client,
     Databases,
     OAuthProvider,
+    Query,
 } from 'react-native-appwrite';
 import * as Linking from 'expo-linking';
 import { openAuthSessionAsync } from 'expo-web-browser';
+import { build } from '@expo/plist/build/build';
 
 export const config = {
     platform: 'com.nk.trueEstate',
     endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
     projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
-    databaseId: process.env.EXPO_PUBLIC_APPWRITE_DATABASE,
+    databaseId: process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID,
     galleriesCollectionId:
         process.env.EXPO_PUBLIC_APPWRITE_GALLERIES_COLLECTION_ID,
     reviewsCollectionId: process.env.EXPO_PUBLIC_APPWRITE_REVIEWS_COLLECTION_ID,
-    agentCollectionId: process.env.EXPO_PUBLIC_APPWRITE_AGENT_COLLECTION_ID,
+    agentsCollectionId: process.env.EXPO_PUBLIC_APPWRITE_AGENT_COLLECTION_ID,
     propertiesCollectionId:
         process.env.EXPO_PUBLIC_APPWRITE_PROPERTIES_COLLECTION_ID,
 };
@@ -30,7 +32,7 @@ client
 
 export const avatar = new Avatars(client);
 export const account = new Account(client);
-export const database = new Databases(client);
+export const databases = new Databases(client);
 
 export async function login() {
     try {
@@ -94,5 +96,74 @@ export async function getCurrentUser() {
     } catch (error) {
         console.log(error);
         return false;
+    }
+}
+
+export async function getLatestProperties() {
+    try {
+        const result = await databases.listDocuments(
+            config.databaseId!,
+            config.propertiesCollectionId!,
+            [Query.orderAsc('$createdAt'), Query.limit(5)]
+        );
+        return result.documents;
+    } catch (error) {
+        console.log(error);
+        return [];
+    }
+}
+
+export async function getProperties({
+    filter,
+    query,
+    limit,
+}: {
+    filter: string;
+    query: string;
+    limit?: number;
+}) {
+    try {
+        const buildQuery = [Query.orderDesc('$createdAt')];
+
+        if (filter && filter !== 'All') {
+            buildQuery.push(Query.equal('type', filter));
+        }
+        if (query) {
+            buildQuery.push(
+                Query.or([
+                    Query.search('name', query),
+                    Query.search('address', query),
+                    Query.search('type', query),
+                ])
+            );
+        }
+
+        if (limit) {
+            buildQuery.push(Query.limit(limit));
+        }
+
+        const result = await databases.listDocuments(
+            config.databaseId!,
+            config.propertiesCollectionId!,
+            buildQuery
+        );
+        return result.documents;
+    } catch (error) {
+        console.error(error);
+        return [];
+    }
+}
+
+export async function getPropertyById({ id }: { id: string }) {
+    try {
+        const result = await databases.getDocument(
+            config.databaseId!,
+            config.propertiesCollectionId!,
+            id
+        );
+        return result;
+    } catch (error) {
+        console.error(error);
+        return null;
     }
 }
